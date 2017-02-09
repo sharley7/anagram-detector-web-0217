fa21992750c1fa6e297df2ec21d3e9fe72b65c96
Siobhan <siobhan.harley@gmail.com>
2017-02-08 22:42:40 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize (word)
+  @word = word
+end
+
+def match (array)
+  match_arr = []
+
+  array.map do | item |
+       arr_one = item.split("").sort.join
+       arr_two = word.split("").sort.join
+       if arr_one == arr_two
+         match_arr << item
+       end
+    end
+    match_arr
+  end
+
+end
